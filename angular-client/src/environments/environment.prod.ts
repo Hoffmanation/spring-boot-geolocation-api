01f8438828b9dccda99dceb31ceee4d0bdf4905d
Oren Hoffman <orenh@paygilant.com>
2020-10-01 10:42:14 +0300

    Changing url + port at env's and H2 DB configs

--- angular-client/src/environments/environment.prod.ts
+++ angular-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://oren-hoffman.com',
-  port: '80',
-  endpoints: { getLocation: 'geo-resolver/secured/get-location'}
+  port: '443',
+  endpoints: { getLocation: '/geo-resolver/secured/get-location'}
 };
